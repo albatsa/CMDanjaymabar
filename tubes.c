398c6d99ea7e97faa645a86401da456e385b56db
wangga03 <aswanggasadu@gmail.com>
2024-03-21 03:34:45 +0700

    menambah fungsi login, regis, dan bikin menu di main

--- tubes.c
+++ tubes.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void regis(){
+    system("cls");
+    printf("Ini Registrasi");
+}
+void login(){
+    system("cls");
+    printf("Ini Login");
+}
+
 int main(){
 
     int opsi;
-    printf("###################################");
-    printf("\n#     D ' F R A M E C R A F T     #\n");
-    printf("###################################\n\n");
+    system("cls");
+    printf("#################################################");
+    printf("\n#            D ' F R A M E C R A F T            #\n");
+    printf("#################################################\n\n");
 
-    printf("||\t1. Sign Out\t||");
-    printf("\t2. Sign In\t||\n> ");
+    printf("||\t1. Registrasi\t||");
+    printf("\t2. Login\t||\n> ");
 
-    scanf("%d", &opsi);
+    scanf("%d", &opsi);fflush(stdin);
+
+    switch (opsi)
+    {
+    case 1:
+        regis();
+        break;
+    case 2:
+        login();
+        break;
     
-
-
+    default:
+        printf("Pilihan tidak ada!!!!!!!!!!!!\n"); system("pause");
+        main();
+        break;
+    }
+    
 }

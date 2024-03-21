2f3cc73a75f3c5e0a959060c1b34acbc1d673b46
wangga03 <aswanggasadu@gmail.com>
2024-03-21 10:17:45 +0700

    menu awal

---
+++ tubes.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int opsi;
+    printf("###################################");
+    printf("\n#     D ' F R A M E C R A F T     #\n");
+    printf("###################################\n\n");
+
+    printf("||\t1. Sign Out\t||");
+    printf("\t2. Sign In\t||\n> ");
+
+    scanf("%d", &opsi);
+    
+
+
+}
